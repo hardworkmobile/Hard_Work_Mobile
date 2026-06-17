0eacdd97fc09087019dbcc63e22c28dc3076b8ee
James Ferzanden <jamesfernandezone@gmail.com>
2026-06-17 10:12:56 -0400

    fix: add Router wrapper and Jest ESM config for smoke test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders without crashing', () => {
-  render(<App />);
+  render(<MemoryRouter><App /></MemoryRouter>);
 });
